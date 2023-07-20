8c22454b9bcd94fa502505d7c9a09332ac938274
nifemi daniel <103764624+Daniel22022@users.noreply.github.com>
2023-07-20 13:51:24 +0100

    new commit

--- accounts/views.py
+++ accounts/views.py
@@ -8,16 +8,16 @@ from django.http import HttpResponse
 
 def login_user(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
+        fullname = request.POST.get('fullname')
+        phone_number = request.POST.get('phone_number')
+        user = authenticate(fullname=fullname, phone_number=phone_number)
 
         if user is not None:
             login(request, user)
             redirect_url = request.GET.get('next', 'home')
             return redirect(redirect_url)
         else:
-            messages.error(request, "Username Or Password is incorrect!",
+            messages.error(request, "Full Name Or PhoneNumber is incorrect!",
                            extra_tags='alert alert-warning alert-dismissible fade show')
 
     return render(request, 'accounts/login.html')
@@ -32,36 +32,31 @@ def create_user(request):
     if request.method == 'POST':
         check1 = False
         check2 = False
-        check3 = False
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password1 = form.cleaned_data['password1']
-            password2 = form.cleaned_data['password2']
-            email = form.cleaned_data['email']
+            fullname = form.cleaned_data['fullname']
+            phone_number1 = form.cleaned_data['phone_number1']
+            phone_number2 = form.cleaned_data['phone_number2']
 
-            if password1 != password2:
+            if phone_number1 != phone_number2:
                 check1 = True
-                messages.error(request, 'Password did not match!',
+                messages.error(request, 'Phone Number did not match!',
                                extra_tags='alert alert-warning alert-dismissible fade show')
-            if User.objects.filter(username=username).exists():
+            if User.objects.filter(fullname=fullname).exists():
                 check2 = True
-                messages.error(request, 'Username already exists!',
-                               extra_tags='alert alert-warning alert-dismissible fade show')
-            if User.objects.filter(email=email).exists():
-                check3 = True
-                messages.error(request, 'Email already registered!',
+                messages.error(request, 'Full Name already exists!',
                                extra_tags='alert alert-warning alert-dismissible fade show')
 
-            if check1 or check2 or check3:
+            if check1 or check2:
                 messages.error(
                     request, "Registration Failed!", extra_tags='alert alert-warning alert-dismissible fade show')
                 return redirect('accounts:register')
             else:
                 user = User.objects.create_user(
-                    username=username, password=password1, email=email)
+                    fullname=fullname, phone_number=phone_number1)
                 messages.success(
-                    request, f'Thanks for registering {user.username}.', extra_tags='alert alert-success alert-dismissible fade show')
+                    request, f'Thanks for registering {user.fullname}.', extra_tags='alert alert-success '
+                                                                                    'alert-dismissible fade show')
                 return redirect('accounts:login')
     else:
         form = UserRegistrationForm()

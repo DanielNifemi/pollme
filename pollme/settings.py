7835910abe0ca2409b8c8d0b08ffde97cd99cbb1
nifemi daniel <103764624+Daniel22022@users.noreply.github.com>
2023-07-20 12:26:05 +0100

    new commit

--- pollme/settings.py
+++ pollme/settings.py
@@ -17,7 +17,6 @@ import accounts.apps
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ SECRET_KEY = 'django-insecure-u&nv72&(tkzqlgfmy5$r3t=@te3auafv+$f*2oksc#$sf=d865
 DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
-
 
 # Application definition
 
@@ -74,20 +72,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pollme.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'version': '3.8.3',  # Use a compatible version here
-        },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'wg2dOMeTa51tDEVdmYRa',
+        'HOST': 'containers-us-west-33.railway.app',
+        'PORT': '5948',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -107,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -118,7 +114,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
